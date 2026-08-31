e652492ae0889434310b822f714569fdf6d0ca90
codejota <102438579+codejota@users.noreply.github.com>
2026-08-31 12:31:00 -0300

    Update Jorlas to 0.4.32

--- Casks/jorlas.rb
+++ Casks/jorlas.rb
@@ -1,8 +1,8 @@
 cask "jorlas" do
-  version "0.4.31"
-  sha256 "dc5280645307a88c095ee24db48cb43b5ddeda914bec0e99a3831e2d99e854f7"
+  version "0.4.32"
+  sha256 "97098f959ca57228f92377dc73696836442e5799bed6ec4c278ef6071189bdf2"
 
-  url "https://raw.githubusercontent.com/codejota/homebrew-tap/main/bootstrap/jorlas-0.4.31-installer.tar.gz"
+  url "https://raw.githubusercontent.com/codejota/homebrew-tap/main/bootstrap/jorlas-0.4.32-installer.tar.gz"
   name "Jorlas"
   desc "Native macOS control desk and menu bar toolkit"
   homepage "https://github.com/codejota/jorlas"
